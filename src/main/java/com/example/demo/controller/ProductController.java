9c09203cdaa209479d9c1ad1018162104d515142
amit4996 <amitkumaroffic@gmail.com>
2026-02-04 10:15:00 +0530

    patch changes

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -36,6 +36,7 @@ public class ProductController {
     @PostMapping("/product")
     public ResponseEntity<Product> addProduct(@Valid @RequestBody Product product){
          Product createdProduct = productService.addProduct(product);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
     }
 
