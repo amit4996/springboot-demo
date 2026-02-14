37bdf72705a7b72dd051d4501ea14c0bbfb0be09
amit4996 <amitkumaroffic@gmail.com>
2026-02-14 10:15:00 +0530

    adding changes

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.example.demo.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,5 +17,15 @@ public class GlobalExceptionHandler {
     HttpServletRequest request){
           Error error = new Error(HttpStatus.NOT_FOUND.value(), exception.getMessage(),request.getRequestURI());
           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Error> handleValidationExceptions(MethodArgumentNotValidException exception,
+                                                            HttpServletRequest request){
+
+        String message = exception.getBindingResult().getFieldErrors().stream().findFirst().map(FieldError::getDefaultMessage).orElse("Validation Error");
+
+        Error error = new Error(HttpStatus.BAD_REQUEST.value(),message,request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -3,6 +3,10 @@ package com.example.demo.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -10,9 +14,13 @@ import lombok.Data;
 @Table (name="Product")
 public class Product {
     @Id
+    @NotNull(message = "Product ID cannot be null")
     private Integer productId;
 
+    @NotBlank(message = "Product name cannot be blank")
+    @Size(min = 2, max = 100, message = "Product name must be between 2 and 100 characters")
     private String productName;
 
+    @Min(value = 0, message = "Price must be zero or positive")
     private int price;
 }
